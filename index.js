a7ad896dac43ba74f5e431421581521cb30b05ea
jk-sourcecode <kabil.jayaraman@emirates.com>
2018-01-10 15:13:06 +0400

    Added swagger

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config()
+const swaggerTools = require('swagger-tools')
+const YAML = require('yamljs')
 
 const app = express()
 
@@ -11,6 +13,12 @@ app.use(bodyParser.urlencoded({
     console.log(`Body-Parser: error parsing input -  ${err}`)
   },
 }))
+
+const swaggerDoc = YAML.load('swagger.yaml')
+swaggerTools.initializeMiddleware(swaggerDoc, (middleware) => {
+  // Serve the Swagger documents and Swagger UI
+  app.use(middleware.swaggerUi())
+})
 
 require('./src/routes')(app)
 
